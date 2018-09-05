5c6a1697b18f7dfe185ac7b39a9f0e4556c5a787
egalland <emmanuel.galland117@gmail.com>
2018-09-05 14:43:14 +0200

    modif tp2

--- part10/TP2/index.php
+++ part10/TP2/index.php
@@ -8,38 +8,59 @@
 A la validation, les données saisies devront aparaitre sous le formulaire. 
 Attention les données devront rester dans les différents éléments du formulaire même après la validation. -->
 <?php
+//import
 include 'regex.php';
-$lastname = '';
-$gender = '';
-$firstname = '';
-$age = '';
-$society = '';
-!empty($_POST['gender'])? $gender = $_POST['gender'] : $gender = $gender;
-!empty($_POST['lastname'])? $lastname = $_POST['lastname'] : $lastname = $lastname;
-!empty($_POST['firstname'])? $firstname = $_POST['firstname'] : $firstname = $firstname;
-!empty($_POST['age'])? $age = $_POST['age'] : $age = $age;
-!empty($_POST['society'])? $society = $_POST['society'] : $society = $society;
+include 'controllerForm.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="utf-8" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" />
         <title>TP 2</title>
     </head>
-    <body>
-        <form enctype="multipart/form-data" action="index.php" method="POST">
-            <p>
-            <select name="gender">
-                <option value="Mr">Mr</option>
-                <option value="Mme">Mme</option>
-            </select>
-                <?= $gender; ?>
-            </p>
-            <p><input type="text" name="lastname" value="<?= $lastname; ?>" /><?= $lastname; ?></p>
-            <p><input type="text" name="firstname" value="<?= $firstname; ?>" /><?= $firstname; ?></p>
-            <p><input type="text" name="age" value="<?= $age; ?>" /><?= $age; ?></p>
-            <p><input type="text" name="society" value="<?= $society; ?>" /><?= $society; ?></p>
-            <input type="submit" value="Envoyer" />
+    <body class="container">
+        <form action="index.php" method="POST" class="pt-4">
+            <div class="form-group row">
+                <label for="gender" class="col-2 col-form-label">Civilité</label>
+                <div class="col-10">
+                    <select name="gender" id="gender" class="custom-select">
+                        <option selected disabled>Selectionner une civilité</option>
+                        <option value="M" <?php echo $gender == 'M' ? 'selected' : ''; ?>>M</option>
+                        <option value="Mme" <?php echo $gender == 'Mme' ? 'selected' : ''; ?>>Mme</option>
+                    </select>
+                    <?php if(!empty($_POST)){ ?><p class="alert alert-danger"><?= empty($errorLog['gender'])? $gender : $errorLog['gender']; ?></p><?php } ?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="lastname" class="col-2 col-form-label">Nom</label>
+                <div class="col-10">
+                    <input class="form-control" type="text" id="lastname" name="lastname" value="<?= $lastname; ?>" />
+                    <?php if(!empty($_POST)){ ?><p class="alert alert-danger"><?= empty($errorLog['lastname'])? $lastname : $errorLog['lastname']; ?></p><?php } ?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="firstname" class="col-2 col-form-label">Prénom</label>
+                <div class="col-10">
+                    <input class="form-control" id="firstname" type="text" name="firstname" value="<?= $firstname; ?>" />
+                    <?php if(!empty($_POST)){ ?><p class="alert alert-danger"><?= empty($errorLog['firstname'])? $firstname : $errorLog['firstname']; ?></p><?php } ?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="age" class="col-2 col-form-label">Age</label>
+                <div class="col-10">
+                    <input class="form-control" id="age" type="text" name="age" value="<?= $age; ?>" />
+                    <?php if(!empty($_POST)){ ?><p class="alert alert-danger"><?= empty($errorLog['age'])? $age : $errorLog['age']; ?></p><?php } ?>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="society" class="col-2 col-form-label">Société</label>
+                <div class="col-10">
+                    <input class="form-control" id="society" type="text" name="society" value="<?= $society; ?>" />
+                    <?php if(!empty($_POST)){ ?><p class="alert alert-danger"><?= empty($errorLog['society'])? $society : $errorLog['society']; ?></p><?php } ?>
+                </div>
+            </div>
+                <input type="submit" value="Envoyer" name="submitForm" class="col btn btn-dark" />
         </form>
     </body>
 </html>
--- part10/TP3/index.php
+++ part10/TP3/index.php
@@ -10,14 +10,7 @@
 
 Les afficher tous sur une même page. -->
 <?php
-$portrait1 = array('name' => 'Victor', 'firstname' => 'Hugo', 'portrait' => 'http://upload.wikimedia.org/wikipedia/commons/5/5a/Bonnat_Hugo001z.jpg');
-$portrait2 = array('name' => 'Jean', 'firstname' => 'de La Fontaine', 'portrait' => 'http://upload.wikimedia.org/wikipedia/commons/e/e1/La_Fontaine_par_Rigaud.jpg');
-$portrait3 = array('name' => 'Pierre', 'firstname' => 'Corneille', 'portrait' => 'http://upload.wikimedia.org/wikipedia/commons/2/2a/Pierre_Corneille_2.jpg');
-$portrait4 = array('name' => 'Jean', 'firstname' => 'Racine', 'portrait' => 'http://upload.wikimedia.org/wikipedia/commons/d/d5/Jean_racine.jpg');
-
-function showArray($array) {
-    echo $array['name'] . ' ' . $array['firstname'] . ' ' . $array['portrait'];
-}
+include 'functionArray.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -26,8 +19,8 @@ function showArray($array) {
         <title>TP 3</title>
     </head>
     <body>
-    <?php for ($i = 1; $i <= 4; $i++) { ?>
-        <p><?php showArray(${'portrait' . $i}); ?></p>
-    <?php } ?>
-</body>
+        <?php for ($i = 1; $i <= 4; $i++) { ?>
+            <p><?php showArray(${'portrait' . $i}); ?></p>
+        <?php } ?>
+    </body>
 </html>
